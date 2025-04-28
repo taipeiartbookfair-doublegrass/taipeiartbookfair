c18f2b3e2d4d36a7e0937a0b2df9b39ab97ea7f0
Imisky <yehforcode@gmail.com>
2025-04-28 11:34:58 +0800

    fixbug

--- uploadoversea.js
+++ uploadoversea.js
@@ -52,7 +52,7 @@ document
           redirect: "follow",
           method: "POST",
           headers: {
-            "Content-Type": "application/json",
+            "Content-Type": "text/plain;charset=utf-8",
           },
           body: formData,
         }
