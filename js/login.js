1e7d199cc4a00f67fd27473f6b7efd2c750231a6
Pat Yeh <yehforcode@gmail.com>
2025-05-18 15:10:08 +0800

    Refactor login form submission to use async/await and improve error handling

--- js/login.js
+++ js/login.js
@@ -3,15 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
     "https://script.google.com/macros/s/AKfycbwNWgPsLK_ldHUIvoIg5a9k3PNIlmjvJeTgbCZ5CZsvKFQ7e1DoxbMsAawi4nI3Rea4DA/exec";
   const form = document.querySelector(".login-form");
 
-  form.addEventListener("submit", function (e) {
+  form.addEventListener("submit", async function (e) {
     e.preventDefault();
 
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value;
 
-    //
     if (!email || !password) {
-      alert("please enter your email and password.");
+      alert("Please enter your email and password.");
       return;
     }
 
@@ -19,31 +18,37 @@ document.addEventListener("DOMContentLoaded", function () {
       action: "login",
       account: email,
       password: password,
-    });
+    }).toString;
 
-    fetch(`${apiUrl}?${params.toString()}`, {
-      method: "POST",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.success) {
-          // cookie：6hr
-          setCookieSec("account", data.data.account, 21600);
-          setCookieSec("region", data.data.region, 21600);
-          setCookieSec("login", "success", 21600);
-
-          //   alert("登入成功！Login successful!");
-          // redirect to dashboard
-          window.location.href = "dashboard-TPABF.html";
-        } else {
-          alert(
-            data.message || data.error || "Login failed, please try again."
-          );
-        }
-      })
-      .catch((err) => {
-        alert("Server error, please try again later.");
-        console.error(err);
+    try {
+      const res = await fetch(uploadUrl, {
+        redirect: "follow",
+        method: "POST",
+        headers: {
+          "Content-Type": "text/plain;charset=utf-8",
+        },
+        body: params,
       });
+      if (!res.ok) {
+        alert("Server error, please try again later.");
+        return;
+      }
+
+      const data = await res.json();
+
+      if (data.success) {
+        setCookieSec("account", data.data.account, 21600);
+        setCookieSec("region", data.data.region, 21600);
+        setCookieSec("login", "success", 21600);
+
+        // alert("登入成功！Login successful!");
+        window.location.href = "dashboard-TPABF.html";
+      } else {
+        alert(data.message || data.error || "Login failed, please try again.");
+      }
+    } catch (error) {
+      alert("Network error, please try again later.");
+      console.error(error);
+    }
   });
 });
