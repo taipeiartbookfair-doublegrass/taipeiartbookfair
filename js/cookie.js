f5e31d930596d6de82418af025da59557e077ae2
Pat Yeh <yehforcode@gmail.com>
2025-05-18 14:55:02 +0800

    Add login functionality with form validation and API integration

---
+++ js/cookie.js
@@ -0,0 +1,23 @@
+function setCookie(name, value, seconds) {
+  let expires = "";
+  if (seconds) {
+    const date = new Date();
+    date.setTime(date.getTime() + seconds * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie =
+    name + "=" + encodeURIComponent(value) + expires + "; path=/";
+}
+
+// 取得 cookie
+function getCookie(name) {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0)
+      return decodeURIComponent(c.substring(nameEQ.length, c.length));
+  }
+  return null;
+}
